8fb96de6d79aa42514426059896ca513f88e46ec
joshadek <161732473+joshadek@users.noreply.github.com>
2026-04-21 13:28:24 +0100

    Add files via upload

---
+++ ML Projects/Streamlit Regression Project-Best/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.set_page_config(page_title= "California Housing Regression Model", layout="wide")
+
+st.title("California housing price predictor")
+st.markdown("""
+Navigation:
+            
+
+1. **Housing Data**
+2. **Train**
+3. **Evaluate**
+4. **Predict**
+
+""")
